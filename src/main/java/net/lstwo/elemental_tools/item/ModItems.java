cb098df01600d6e7b12faf0c5da1bd865e3b9c17
LeShaDDoW <leshaddow3@gmail.com>
2023-06-03 16:33:48 +0200

    added shovel textures and names

--- src/main/java/net/lstwo/elemental_tools/item/ModItems.java
+++ src/main/java/net/lstwo/elemental_tools/item/ModItems.java
@@ -10,6 +10,10 @@ import net.lstwo.elemental_tools.item.pickaxe.AirPickaxe;
 import net.lstwo.elemental_tools.item.pickaxe.EarthPickaxe;
 import net.lstwo.elemental_tools.item.pickaxe.FirePickaxe;
 import net.lstwo.elemental_tools.item.pickaxe.WaterPickaxe;
+import net.lstwo.elemental_tools.item.shovel.AirShovel;
+import net.lstwo.elemental_tools.item.shovel.EarthShovel;
+import net.lstwo.elemental_tools.item.shovel.FireShovel;
+import net.lstwo.elemental_tools.item.shovel.WaterShovel;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ToolMaterials;
@@ -22,16 +26,19 @@ import java.util.List;
 public class ModItems {
     static List<FullItem> itemList = new ArrayList<>();
 
-        // Pickaxes
+    public static final Item FIRE_SHOVEL = createCustomItem("fire_shovel", new FireShovel(ToolMaterials.DIAMOND));
     public static final Item FIRE_PICKAXE = createCustomItem("fire_pickaxe", new FirePickaxe(ToolMaterials.DIAMOND));
     public static final Item FIRE_AXE = createCustomItem("fire_axe", new FireAxe(ToolMaterials.DIAMOND));
 
+    public static final Item WATER_SHOVEL = createCustomItem("water_shovel", new WaterShovel(ToolMaterials.GOLD));
     public static final Item WATER_PICKAXE = createCustomItem("water_pickaxe", new WaterPickaxe(ToolMaterials.GOLD));
     public static final Item WATER_AXE = createCustomItem("water_axe", new WaterAxe(ToolMaterials.GOLD));
 
+    public static final Item EARTH_SHOVEL = createCustomItem("earth_shovel", new EarthShovel(ToolMaterials.IRON));
     public static final Item EARTH_PICKAXE = createCustomItem("earth_pickaxe", new EarthPickaxe(ToolMaterials.IRON));
     public static final Item EARTH_AXE = createCustomItem("earth_axe", new EarthAxe(ToolMaterials.IRON));
 
+    public static final Item AIR_SHOVEL = createCustomItem("air_shovel", new AirShovel(ToolMaterials.DIAMOND));
     public static final Item AIR_PICKAXE = createCustomItem("air_pickaxe", new AirPickaxe(ToolMaterials.DIAMOND));
     public static final Item AIR_AXE = createCustomItem("air_axe", new AirAxe(ToolMaterials.DIAMOND));
 
